2ab0ce507a9b6b5b033f6592a241d469c295a701
Erickkaranja <karanja99erick@gmail.com>
2023-03-30 10:57:36 +0300

    correction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,20 +8,14 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height, max;
 
-	if (tree == NULL)
+	if (tree)
 	{
-		return (0);
-	}
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	if (right_height > left_height)
-	{
-		max = right_height;
-	}
-	else
-		max = left_height;
+		size_t l = 0, r = 0;
 
-	return (max + 1);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
