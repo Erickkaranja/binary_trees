a3d252084180915d33732220ff32eff4a971a3e4
Erickkaranja <karanja99erick@gmail.com>
2023-03-30 10:12:34 +0300

    correction

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,10 +17,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	if (parent == NULL)
-	{
-		return (NULL);
-	}
+
 	if (parent->left != NULL)
 	{
 		parent->left->parent = leftNode;
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,16 +9,12 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *rightNode;
 
-	if (parent == NULL)
-	{
-		return (NULL);
-	}
 	rightNode = binary_tree_node(parent, value);
 	if (rightNode == NULL)
 	{
 		return (NULL);
 	}
-	if (rightNode->right != NULL)
+	if (parent->right != NULL)
 	{
 		rightNode->right = parent->right;
 		parent->right->parent = rightNode;
