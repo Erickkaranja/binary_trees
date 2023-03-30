4b0a91131f68c254ce70edc7dbd43ca8c7a8b6ea
Erickkaranja <karanja99erick@gmail.com>
2023-03-30 10:28:15 +0300

    correcting task 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,13 +8,9 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL)
+	if (node->left != NULL || node->right != NULL || node == NULL)
 	{
 		return (0);
 	}
-	if (node->left == NULL && node->right == NULL && node->parent != NULL)
-	{
-		return (1);
-	}
-	return (0);
+	return (1);
 }
