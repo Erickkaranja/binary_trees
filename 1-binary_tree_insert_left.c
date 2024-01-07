d9eb4a49311c8a4c094f4747df72d4cff7934cfb
Erickkaranja <karanja99erick@gmail.com>
2024-01-07 21:05:41 +0300

    adding a check if parent is Null

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,6 +10,11 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
 	binary_tree_t *leftNode;
 
 	leftNode = binary_tree_node(parent, value);
