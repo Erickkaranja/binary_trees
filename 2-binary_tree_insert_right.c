ffbdce1e49f53d5b40256fa29cf06481d09b1c33
Erickkaranja <karanja99erick@gmail.com>
2024-01-08 14:48:00 +0300

    updating condition for a null parent

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,6 +15,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
+	if (parent == NULL)
+		return (NULL);
 	if (parent->right != NULL)
 	{
 		rightNode->right = parent->right;
